b26d7bbdc9e331dd78bbed5b354935f65a166d84
Maren Gjesing Laursen <mare0231@edu.campusvejle.dk>
2020-02-03 14:58:45 +0100

    Project S3WeatherApp.Services created with Client class

---
+++ S3WeatherApp.Services/Client.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S3WeatherApp.Services
+{
+    public class Client
+    {
+        string apiKey;
+        string formatMode;
+        string serverUrl;
+
+        Task<WeatherData> GetCurrentTemperature(string city)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
