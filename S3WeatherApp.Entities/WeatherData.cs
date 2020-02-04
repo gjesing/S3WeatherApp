ca215b1bae90a328fe16912d2ba67539b6bb7357
Maren Gjesing Laursen <mare0231@edu.campusvejle.dk>
2020-02-04 12:01:23 +0100

    Method GetCurrentTemperature(string city) created in the Client class. The method gets the temperature from the API using the cityname and returns an instance of WeatherData

---
+++ S3WeatherApp.Entities/WeatherData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S3WeatherApp.Entities
+{
+    public struct WeatherData
+    {
+        private readonly string city;
+        private readonly string temperature;
+
+        public WeatherData(string city, string temperature)
+        {
+            this.city = city;
+            this.temperature = temperature;
+        }
+
+        public string City { get { return city; } }
+        public string Temperature { get { return temperature; } }
+    }
+}
--- S3WeatherApp.Services/Client.cs
+++ S3WeatherApp.Services/Client.cs
@@ -1,20 +1,59 @@
-﻿using System;
+﻿using S3WeatherApp.Entities;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace S3WeatherApp.Services
 {
     public class Client
     {
-        string apiKey;
-        string formatMode;
-        string serverUrl;
+        private const string apiKey = "be3af3fd5849838c549f1d557af0e382";
+        private const string formatMode = "xml";
+        private const string serverUrl = "api.openweathermap.org/data/2.5/weather";
 
         Task<WeatherData> GetCurrentTemperature(string city)
         {
-            throw new NotImplementedException();
+            return Task<WeatherData>.Factory.StartNew(() =>
+            {
+                string apiOutput = "";
+                UriBuilder uriBuilder = new UriBuilder(serverUrl);
+                uriBuilder.Query = $"q={city},dk&units=metric&appid={apiKey}";
+                WebRequest request = WebRequest.Create(uriBuilder.Uri);
+                request.Timeout = 5000;
+                try
+                {
+                    WebResponse response = request.GetResponse();
+                    using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                    {
+                        apiOutput = reader.ReadToEnd();
+                    }
+                }
+                catch (WebException ex) when (ex.Status == WebExceptionStatus.Timeout)
+                {
+                    throw new TimeoutException("Request took to long");
+                }
+                catch (WebException ex)
+                {
+                    throw new HttpListenerException(ex.HResult, "Request failed");
+                }
+                XmlDocument xml = new XmlDocument();
+                try
+                {
+                    xml.LoadXml(apiOutput);
+                }
+                catch
+                {
+                    throw new FormatException("API returned data in invalid format.");
+                }
+                XmlNode temperatureNode = xml.SelectSingleNode("current").SelectSingleNode("temperature");
+                string temperature = temperatureNode.Attributes.GetNamedItem("value").Value;
+                return new WeatherData(city, temperature);
+            });
         }
     }
 }
